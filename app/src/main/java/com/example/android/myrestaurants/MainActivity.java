ab399c144c220ad359f0ef627da88255a5a1e090
Brent Perry <burentoperii@gmail.com>
2017-02-14 10:47:17 -0600

    add textView to Restaurant activity to display input zip code.

--- app/src/main/java/com/example/android/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/android/myrestaurants/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.android.myrestaurants;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
