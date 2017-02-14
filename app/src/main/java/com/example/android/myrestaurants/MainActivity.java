25ddbf7cf6cbe56f866c45f3fe79cc52fe4493b0
Brent Perry <burentoperii@gmail.com>
2017-02-14 09:54:43 -0600

    replace onClick toast with intent to new activity.

--- app/src/main/java/com/example/android/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/android/myrestaurants/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.android.myrestaurants;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
@@ -18,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
             }
         });
     }
