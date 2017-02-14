5dd3d52d4f9b1572f7de0ecb0e1d3ab5985b3f4c
Brent Perry <burentoperii@gmail.com>
2017-02-14 09:45:50 -0600

    add onClickListener to button, to display toast.

--- app/src/main/java/com/example/android/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/android/myrestaurants/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android.myrestaurants;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
@@ -13,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
